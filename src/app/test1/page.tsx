e7f3f3cf47a116ab479ae393506c9daef5ea7408
Kinetic639 <michal.stepien36@gmail.com>
2024-03-03 11:05:36 +0100

    added single page dynamic route

--- src/app/test1/page.tsx
+++ src/app/test1/page.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from "react";
+import { ProductCounter } from "@/ui/atoms/ProductCounter";
 
 const Page = () => {
-    return (
-        <div>Page1</div>
-    )
-}
-export default Page
+	return (
+		<div>
+			<ProductCounter />
+		</div>
+	);
+};
+export default Page;
